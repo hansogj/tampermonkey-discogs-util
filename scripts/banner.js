a7298dcdbf5408c7079f6192f4879bc3382ac9ee
Hans Ole Gjerdrum <hansogj@gmail.com>
2025-12-07 17:25:59 +0100

    docs(@updateURL@downloadURL): adding to banner

--- scripts/banner.js
+++ scripts/banner.js
@@ -3,6 +3,8 @@
 // @namespace    @hansogj
 // @version      {VERSION_PLACEHOLDER}
 // @description  https://github.com/hansogj/tampermonkey-discogs-util.  Adds a sticky panel to Discogs with grading, location, "In collection" filter, and "Unique items" filter, with dark theme. Now with dynamic loading of all custom fields via API token.
+// @updateURL    https://raw.githubusercontent.com/hansogj/tampermonkey-discogs-util/main/dist/index.js                                                                                                                                            │
+// @downloadURL  https://raw.githubusercontent.com/hansogj/tampermonkey-discogs-util/main/dist/index.js                                                                                                                                            │
 // @author       hansogj@gmail.com
 // @match        https://www.discogs.com/*
 // @grant        GM_xmlhttpRequest
